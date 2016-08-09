de0b8ed10da356015b09620d0b14f960244762a1
Miguh Ruiz <miguhruiz@gmail.com>
2016-08-09 12:09:01 +0200

    Created tweet class

---
+++ models/Tweet.h
@@ -0,0 +1,13 @@
+//
+//  Tweet.h
+//  PlatziObjectiveC
+//
+//  Created by Miguh Ruiz on 9/8/16.
+//  Copyright © 2016 Miguh Ruiz. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Tweet : NSObject
+
+@end
---
+++ models/Tweet.m
@@ -0,0 +1,13 @@
+//
+//  Tweet.m
+//  PlatziObjectiveC
+//
+//  Created by Miguh Ruiz on 9/8/16.
+//  Copyright © 2016 Miguh Ruiz. All rights reserved.
+//
+
+#import "Tweet.h"
+
+@implementation Tweet
+
+@end
