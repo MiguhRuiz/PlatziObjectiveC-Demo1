38f67d58f66d92599dbc96e06a5a1e40a0cc6805
Miguh Ruiz <miguhruiz@gmail.com>
2016-08-09 10:22:19 +0200

    Initial Commit

---
+++ PlatziObjectiveC/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  PlatziObjectiveC
+//
+//  Created by Miguh Ruiz on 9/8/16.
+//  Copyright © 2016 Miguh Ruiz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ PlatziObjectiveC/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  PlatziObjectiveC
+//
+//  Created by Miguh Ruiz on 9/8/16.
+//  Copyright © 2016 Miguh Ruiz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
